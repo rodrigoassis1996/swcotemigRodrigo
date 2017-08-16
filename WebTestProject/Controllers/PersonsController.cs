0612ca56bf6ff223ba2f7d4568ada4da4b17918f
71700781 <71700781@OMEGAB07>
2017-08-16 20:04:42 -0300

    Adicionar arquivos de projeto.

---
+++ WebTestProject/Controllers/PersonsController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebTestProject.Models;
+
+namespace WebTestProject.Controllers
+{
+    [RoutePrefix("v1/persons")]
+    public class PersonsController : ApiController
+    {
+        private string Name { get;set; }
+        private static IList<Person> _persons = new List<Person>();
+
+        [Route("{id}/name")]
+        [HttpGet]
+        public string GetName(int id)
+        {
+            Name = "Rodrigo";
+            return Name;
+        }
+
+        [Route("{id}/old")]
+        [HttpGet]
+        public int GetOld(int id)
+        {
+            return id;
+        }
+
+        [Route("{id}/surname")]
+        [HttpGet]
+        public string GetSurName(int id)
+        {
+            return "Assis";
+        }
+
+        [Route("{id}")]
+        [HttpGet]
+        public Person GetPersons(int id)
+        {
+            return _persons[id];
+        }
+
+        [Route]
+        [HttpGet]
+        public IList<Person> GetPersons()
+        {
+            return _persons;
+        }
+
+        [Route]
+        [HttpPost]
+        public Person PostPersons(Person person)
+        {
+            _persons.Add(person);
+            //person.Id = _persons.Count;
+            return person;
+        }
+    }
+}
---
+++ WebTestProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebTestProject.WebApiApplication" Language="C#" %>
